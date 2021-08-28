7c2735f755b9777c65e8604955215fdfe00ac597
Yuyu <swsalim@gmail.com>
2021-08-28 23:45:44 +0800

    added netlify files

--- nuxt.config.js
+++ nuxt.config.js
@@ -89,7 +89,13 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    '@nuxtjs/netlify-files'
+  ],
+
+  netlifyFiles: {
+    existingFilesDirectory: './netlify/'
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
